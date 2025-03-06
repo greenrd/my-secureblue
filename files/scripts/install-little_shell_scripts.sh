f0cb3a2b919bfa90c4cf7ab3e879f0b873b81ed4
Robin Green <greenrd@greenrd.org>
2025-03-06 10:15:13 +0000

    Install little_shell_scripts

---
+++ files/scripts/install-little_shell_scripts.sh
@@ -0,0 +1,5 @@
+#! /bin/bash -peux
+git clone https://github.com/tpo/little_shell_scripts.git
+cd little_shell_scripts
+# These are the ones I want to install, for now
+install wping psa linechop mv_ln kill_ssh file_chooser executable_wrapper cptmp catwhich cat_null_terminated /usr/bin
